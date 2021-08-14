09c4af81b880b609cec18a9f9e787529f8588121
SiddhiGupta19 <88777332+SiddhiGupta19@users.noreply.github.com>
2021-08-14 17:43:07 +0530

    Add files via upload

---
+++ JAVASCRIPT 1/JS/main.js
@@ -0,0 +1,1 @@
+document.write("<h1>Hello ITM 3 sem</h1>")
---
+++ JAVASCRIPT 1/JS/set.js
@@ -0,0 +1,20 @@
+//var element;
+//document.write("hello");
+
+document.getElementById("a").innerHTML = "<center><h1>ITM GWALIOR</h1></center>"
+
+document.getElementById("b").innerHTML = "<h1>Phone:7000846823</h1>"
+
+document.getElementById("a").setAttribute("style","border:10px dotted red");
+document.getElementById("a").removeAttribute("style");
+
+
+
+/////////query selector set or get dono/////////
+
+
+document.querySelector(".c").innerHTML = "<center><h1>WOW</h1></center>"
+document.querySelector("#e").innerHTML = "<center><h1>Taj</h1></center>"
+
+
+//element = document.querySelector("#a");
